dc7587e227396411022de551023ca038037485de
pi3rrot <4dakto@gmail.com>
2018-12-17 21:41:54 +0900

    Add State with the useState Hook to a React Function Component

---
+++ src/SimpleExamples/Playground.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+export default function Playground() {
+  const [state, setState] = useState({
+    text: "",
+    checked: false
+  });
+
+  const mergeState = partialState =>
+    setState(prevState => ({
+      ...prevState,
+      ...partialState
+    }));
+  return (
+    <section>
+      <input
+        type="text"
+        value={state.text}
+        onChange={e => mergeState({ text: e.target.value })}
+      />
+      <input
+        type="checkbox"
+        checked={state.checked}
+        onChange={e => mergeState({ checked: !state.checked })}
+      />
+      <ul>
+        <li>{state.text}</li>
+        <li>{state.checked.toString()}</li>
+      </ul>
+    </section>
+  );
+}
+
+export function PlaygroundX() {
+  return (
+    <section>
+      <input type="text" value={""} onChange={e => {}} />
+      <input type="checkbox" checked={false} onChange={e => {}} />
+      <ul>
+        <li>{""}</li>
+        <li>{""}</li>
+      </ul>
+    </section>
+  );
+}
